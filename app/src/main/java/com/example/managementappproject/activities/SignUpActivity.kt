d3d70a3ab9b9fee789170838843118dac28ecdc2
Federico De Benedictis <66374898+MenneaDb@users.noreply.github.com>
2021-01-25 00:46:59 +0100

    I implemented the registerUser method inside SignUpActivity.kt in order to have the authentication provided from firebase by using email and password

--- app/src/main/java/com/example/managementappproject/activities/SignUpActivity.kt
+++ app/src/main/java/com/example/managementappproject/activities/SignUpActivity.kt
@@ -5,6 +5,8 @@ import android.text.TextUtils
 import android.view.WindowManager
 import android.widget.Toast
 import com.example.managementappproject.R
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
 import kotlinx.android.synthetic.main.activity_sign_up.*
 
 /* I need this class to be able to use all of the functionality of AppCompactActivity
@@ -48,13 +50,31 @@ class SignUpActivity : BaseActivity() {
         val email: String = et_signUp_email.text.toString().trim {it <= ' '}
         val password: String = et_signUp_password.text.toString().trim { it <= ' '}
 
-        // if all the user passed all the values, the user can press signUp and register
         if (validateForm(name, email, password)) {
-            Toast.makeText(
-            this@SignUpActivity,
-            "Welcome to ManageIT",
-            Toast.LENGTH_SHORT
-            ).show()
+            showProgressDialog(resources.getString(R.string.please_wait))
+            FirebaseAuth.getInstance()
+                .createUserWithEmailAndPassword(email, password).addOnCompleteListener { task ->
+                    hideProgressDialog()
+                    if (task.isSuccessful) {
+                        val firebaseUser: FirebaseUser = task.result!!.user!!
+                        val registeredEmail = firebaseUser.email!!
+                        Toast.makeText(
+                            this, "$name you have " +
+                                    "successfully registered the email " +
+                                    "address $registeredEmail",
+                            Toast.LENGTH_LONG
+                        ).show()
+                        FirebaseAuth.getInstance().signOut()
+                        finish()
+                    } else {
+                        Toast.makeText(
+                            this,
+                            task.exception!!.message,
+                            Toast.LENGTH_SHORT
+                        ).show()
+                    }
+                }
+
         }
     }
 
