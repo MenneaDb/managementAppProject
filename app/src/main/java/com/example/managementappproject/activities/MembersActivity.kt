6df38243a5fe11721ee2804783462372d7b20de0
Federico De Benedictis <66374898+MenneaDb@users.noreply.github.com>
2021-02-11 23:39:48 +0100

    implemented the dialog to add a member to the taskList. layout only

--- app/src/main/java/com/example/managementappproject/activities/MembersActivity.kt
+++ app/src/main/java/com/example/managementappproject/activities/MembersActivity.kt
@@ -1,7 +1,11 @@
 package com.example.managementappproject.activities
 
+import android.app.Dialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.managementappproject.R
 import com.example.managementappproject.adapters.MemberListItemsAdapter
@@ -10,6 +14,7 @@ import com.example.managementappproject.models.Board
 import com.example.managementappproject.models.User
 import com.example.managementappproject.utils.Constants
 import kotlinx.android.synthetic.main.activity_members.*
+import kotlinx.android.synthetic.main.dialog_search_member.*
 
 class MembersActivity : BaseActivity() {
 
@@ -56,5 +61,45 @@ class MembersActivity : BaseActivity() {
         }
 
         toolbar_members_activity.setNavigationOnClickListener { onBackPressed() }
+    }
+    // whenever we create a menu we need 2 methods. the 2nd is necessary if we want an event to happen
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        //inflate the menu
+        menuInflater.inflate(R.menu.menu_add_member, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // add onClick event
+        when(item.itemId){
+            R.id.action_add_member -> { // if the user click this View
+                dialogSearchMember()
+                return true // because all went good we can return true
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    // method to create the Dialog
+    private fun dialogSearchMember(){
+        // 1st we create a dialog
+        val dialog = Dialog(this@MembersActivity)
+        // we specify which dialog I want to display
+        dialog.setContentView(R.layout.dialog_search_member)
+        // we need to add the onClickListener for both buttons of the view
+        dialog.tv_add.setOnClickListener{
+            // 1st we need to get the email
+            val email = dialog.et_email_search_member.text.toString()
+            if (email.isNotEmpty()){
+                dialog.dismiss()
+                // TODO adding members logic
+            }else{
+                Toast.makeText(this@MembersActivity, "Please enter members email address", Toast.LENGTH_SHORT).show()
+            }
+        }
+        dialog.tv_cancel.setOnClickListener {
+            dialog.dismiss()
+        }
+        dialog.show() // we need to make sure that the dialog is shown at some point
     }
 }
